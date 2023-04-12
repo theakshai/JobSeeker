318314fb109a956115f4ebfb66d959a6f7136c64
theakshai <akshaiakshai35@gmail.com>
2023-04-12 11:27:08 +0100

    Added Job.cs raw model schema

---
+++ Models/Job.cs
@@ -0,0 +1,15 @@
+namespace JobSeeker.Models
+{
+   
+  public class Job 
+  {
+    public int Id {get;set;}
+    public string Name {get;set;}
+    public string Description {get;set;}
+    public decimal  Salary {get;set;}
+    public string Company {get;set;}
+    public string Area {get;set;}
+    public string Category {get;set;}
+  }
+  
+}
